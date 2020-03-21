f60f61d9b7777c7071357107cf6c79de9b0fd130
millord <juancarlosmillord@gmail.com>
2020-03-21 09:02:40 -0400

    changed the bg color of the title of the app

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import DataFeching from "./components/DataFetching";
 function App() {
   return (
     <div>
-      <h2 className="center-align">Zen Blog</h2>
+      <h2 className="center-align red text-white">Zen Blog</h2>
       <h5 className="center-align">List of Post:</h5>
       <DataFeching />
     </div>
